65cac789bc4e3750f0fa9c1d14b4a999905df744
Juergen Brendel <juergen@brendel.com>
2017-12-06 15:06:36 +1300

    DC support and relative URLs.

--- topowiz/http.py
+++ topowiz/http.py
@@ -20,7 +20,7 @@ import ipaddr
 import json
 import urllib
 
-from flask     import Flask, render_template, request, redirect
+from flask     import Flask, render_template, request, redirect, url_for
 from wtforms   import RadioField, SelectMultipleField, StringField, \
                       SubmitField, IntegerField, validators, widgets
 from flask_wtf import FlaskForm
@@ -105,6 +105,33 @@ HELP_TEXT_NETWORK = \
      "if you have very large numbers of endpoints on hosts to reduce the "
      "number of routes on hosts.")
 
+HELP_TEXT_DC_OWN_PREFIX = \
+    ("If this question is answered with 'Yes' then Romana's IPAM ensures "
+     "that all endpoints assigned to a given host have the same address "
+     "prefix. This is useful if routes need to be set in the network "
+     "in order to reach endoints directly from outside the cluster: "
+     "Giving each host its own "
+     "unique address prefix for all endpoints means that only a "
+     "single route needs to be configured in the network to make all "
+     "endpoints on that host reachable.<br>&nbsp;<br>"
+     "Note that this is usually only necessary if you have special "
+     "requirements or restrictions on your network, since normally Romana "
+     "sets routes directly on the cluster hosts to make endpoints reachable.")
+
+HELP_TEXT_DC_FLAT_NET = \
+    ("If all your hosts are located within the same L2 segment and they "
+     "can all reach each other without intermittent routers or gateways "
+     "then answer this question with 'Yes'.")
+
+HELP_TEXT_DC_RACKS = \
+    ("Information about your network, such as the maximum number of racks "
+     "and (if required) the maximum number of hosts per rack. This is used "
+     "by Romana's IPAM to calculate address prefix groups for efficient route "
+     "aggregation.")
+
+HELP_TEXT_DC_FLAT_NUM_HOSTS = \
+    ("Since you selected to have a prefix group per host, Romana needs to "
+     "know the maximum number of hosts you will have in your cluster.")
 
 def calculate_num_groups(conf, num_networks=None):
     """
@@ -168,18 +195,18 @@ def build_topology(conf):
     else:
         # Routed DC network
         t = {
-            "networks" : [n['name'] for n in conf.networks],
+            "networks" : [n['name'] for n in conf['networks']],
         }
 
         top_level_group_label = None
-        if conf.dc_flat_net:
-            if conf.dc_pg_per_host:
-                num_groups = conf.dc_flat_net_num_hosts
+        if conf['dc_flat_net']:
+            if conf['dc_pg_per_host']:
+                num_groups = conf['dc_flat_net_num_hosts']
                 top_level_group_label = "host-%d"
             else:
                 num_groups = 1
         else:
-            num_groups = conf.dc_num_racks
+            num_groups = conf['dc_num_racks']
             top_level_group_label = "rack-%d"
 
         m = []
@@ -187,14 +214,14 @@ def build_topology(conf):
             g = {"groups" : []}
             if top_level_group_label:
                 g["name"] = top_level_group_label % i
-            if not conf.dc_flat_net:
+            if not conf['dc_flat_net']:
                 g["assignment"] = {"rack" : g["name"]}
             m.append(g)
 
-        if not conf.dc_flat_net:
-            if conf.dc_pg_per_host:
+        if not conf['dc_flat_net']:
+            if conf['dc_pg_per_host']:
                 for top_level_group in m:
-                    for i in range(conf.dc_num_hosts_per_rack):
+                    for i in range(conf['dc_num_hosts_per_rack']):
                         g = {
                             "name" : "host-%d" % i,
                             "groups" : []
@@ -221,10 +248,48 @@ class IsAwsForm(FlaskForm):
     submit = SubmitField(label='Submit')
 
 
+class DcOwnPrefixForm(FlaskForm):
+    dc_pg_per_host = RadioField('Should each host have its own CIDR prefix '
+                                'for endpoint addresses?',
+                                 choices=[("yes", "Yes"), ("no", "No")],
+                                 validators=[validators.DataRequired()])
+    submit = SubmitField(label='Submit')
+
+
+class DcFlatNetForm(FlaskForm):
+    dc_flat_net = RadioField('Do you have a flat network in your data '
+                             'center? Are all hosts on a single L2 segment?',
+                              choices=[("yes", "Yes"), ("no", "No")],
+                              validators=[validators.DataRequired()])
+    submit = SubmitField(label='Submit')
+
+
+class DcFlatNetNumHostsForm(FlaskForm):
+    dc_flat_net_num_hosts = IntegerField(
+                                'Maximum number of hosts in cluster?',
+                                validators=[
+                                    validators.DataRequired(),
+                                    validators.NumberRange(
+                                       message="Should be between 1 and 2048",
+                                       min=1, max=2048)
+                                    ])
+    submit = SubmitField(label='Submit')
+
+
+class DcRacksForm(FlaskForm):
+    dc_num_racks = IntegerField('Maximum number of racks in data center?',
+                                validators=[
+                                    validators.DataRequired(),
+                                    validators.NumberRange(
+                                       message="Should be between 1 and 256",
+                                       min=1, max=256)
+                                    ])
+
+
 class AwsRegionForm(FlaskForm):
     aws_region = RadioField('Select the AWS region of your cluster:',
                             choices=[(r,r) for r in AWS_REGIONS],
-                            validators=[validators.DataRequired()])
+                            validators=[ validators.DataRequired()])
     submit = SubmitField(label='Submit')
 
 
@@ -307,7 +372,7 @@ def home():
 
 
 @app.route('/is_aws', methods=['GET', 'POST'])
-def is_aws_view():
+def is_aws():
     """
     Asking whether this is an AWS or datacenter deployment.
 
@@ -317,18 +382,144 @@ def is_aws_view():
     if form.validate_on_submit():
         conf = { "is_aws" : form.is_aws.data == "aws" }
         if conf['is_aws']:
-            return redirect('/aws/region/%s' % conf_to_url(conf))
+            return redirect(url_for('.aws_region', raw_conf=conf_to_url(conf)))
         else:
-            raise Exception("Not yet implemented!")
+            return redirect(url_for('.dc_own_prefix',
+                                    raw_conf=conf_to_url(conf)))
 
     return render_template('question.html',
                            form=form,
                            help_text=HELP_TEXT_IS_AWS,
-                           action=request.path)
+                           action=url_for('.is_aws'))
+
+@app.route('/dc/own_prefix/<path:raw_conf>', methods=['GET', 'POST'])
+def dc_own_prefix(raw_conf):
+    """
+    Ask if each host should have its own prefix group in a DC deployment.
+
+    """
+    conf, err = get_conf(raw_conf)
+    if err:
+        return err
+
+    form = DcOwnPrefixForm()
+
+    if form.validate_on_submit():
+        conf['dc_pg_per_host'] = form.dc_pg_per_host.data == "yes"
+        return redirect(url_for('.dc_flat_net', raw_conf=conf_to_url(conf)))
+
+    return render_template('question.html',
+                           form=form,
+                           render_conf=render_conf(conf),
+                           help_text=HELP_TEXT_DC_OWN_PREFIX,
+                           action=url_for('.dc_own_prefix',
+                                          raw_conf=raw_conf))
+
+
+@app.route('/dc/flat_net/<path:raw_conf>', methods=['GET', 'POST'])
+def dc_flat_net(raw_conf):
+    """
+    Ask if we have a flat network in the data center.
+
+    """
+    conf, err = get_conf(raw_conf)
+    if err:
+        return err
+
+    form = DcFlatNetForm()
+
+    if form.validate_on_submit():
+        conf['dc_flat_net'] = form.dc_flat_net.data == "yes"
+        if conf['dc_flat_net']:
+            if conf['dc_pg_per_host']:
+                return redirect(url_for('.dc_flat_net_num_hosts',
+                                        raw_conf=conf_to_url(conf)))
+            else:
+                return redirect(url_for('.gen_networks',
+                                        raw_conf=conf_to_url(conf)))
+        else:
+            return redirect(url_for('.dc_racks', raw_conf=conf_to_url(conf)))
+
+    return render_template('question.html',
+                           form=form,
+                           render_conf=render_conf(conf),
+                           help_text=HELP_TEXT_DC_FLAT_NET,
+                           action=url_for('.dc_flat_net',
+                                          raw_conf=raw_conf))
+
+
+@app.route('/dc/racks/<path:raw_conf>', methods=['GET', 'POST'])
+def dc_racks(raw_conf):
+    """
+    How many racks in a routed network?
+
+    """
+    conf, err = get_conf(raw_conf)
+    if err:
+        return err
+
+    class _DcRacksForm(DcRacksForm):
+        pass
+
+    if conf['dc_pg_per_host']:
+        # Need to ask for number of hosts per rack only if a PG per host was
+        # selected.
+        dc_num_hosts_per_rack = IntegerField(
+                                'Maximum number of hosts per rack?',
+                                validators=[
+                                    validators.DataRequired(),
+                                    validators.NumberRange(
+                                       message="Should be between 1 and 256",
+                                       min=1, max=1024)
+                                    ])
+        setattr(_DcRacksForm, "dc_num_hosts_per_rack", dc_num_hosts_per_rack)
+
+    submit = SubmitField(label='Submit')
+    setattr(_DcRacksForm, "submit", submit)
+
+    form = _DcRacksForm()
+
+    if form.validate_on_submit():
+        conf['dc_num_racks'] = form.dc_num_racks.data
+        if conf['dc_pg_per_host']:
+            conf['dc_num_hosts_per_rack'] = form.dc_num_hosts_per_rack.data
+        return redirect(url_for('.gen_networks', raw_conf=conf_to_url(conf)))
+
+    return render_template('question.html',
+                           form=form,
+                           render_conf=render_conf(conf),
+                           help_text=HELP_TEXT_DC_RACKS,
+                           table_title="Information about your data center " \
+                                       "racks:",
+                           action=url_for('.dc_racks', raw_conf=raw_conf))
+
+
+@app.route('/dc/flat_net_num_hosts/<path:raw_conf>', methods=['GET', 'POST'])
+def dc_flat_net_num_hosts(raw_conf):
+    """
+    How many hosts if we are in a flat network?
+
+    """
+    conf, err = get_conf(raw_conf)
+    if err:
+        return err
+
+    form = DcFlatNetNumHostsForm()
+
+    if form.validate_on_submit():
+        conf['dc_flat_net_num_hosts'] = form.dc_flat_net_num_hosts.data
+        return redirect(url_for('.gen_networks', raw_conf=conf_to_url(conf)))
+
+    return render_template('question.html',
+                           form=form,
+                           render_conf=render_conf(conf),
+                           help_text=HELP_TEXT_DC_FLAT_NUM_HOSTS,
+                           action=url_for('.dc_flat_net_num_hosts',
+                                          raw_conf=raw_conf))
 
 
 @app.route('/aws/region/<path:raw_conf>', methods=['GET', 'POST'])
-def aws_region_view(raw_conf):
+def aws_region(raw_conf):
     """
     Asking for the AWS region in which the cluster is deployed.
 
@@ -341,17 +532,18 @@ def aws_region_view(raw_conf):
 
     if form.validate_on_submit():
         conf['aws_region'] = form.aws_region.data
-        return redirect('/aws/zones/%s' % conf_to_url(conf))
+        return redirect(url_for('.aws_zones', raw_conf=conf_to_url(conf)))
 
     return render_template('question.html',
                            form=form,
                            render_conf=render_conf(conf),
                            help_text=HELP_TEXT_AWS_REGIONS,
-                           action=request.path)
+                           action=url_for('.aws_region',
+                                          raw_conf=raw_conf))
 
 
 @app.route('/aws/zones/<path:raw_conf>', methods=['GET', 'POST'])
-def aws_zones_view(raw_conf):
+def aws_zones(raw_conf):
     """
     Asking for the AWS Zones in which the cluster is deployed.
 
@@ -384,13 +576,14 @@ def aws_zones_view(raw_conf):
 
     if form.validate_on_submit():
         conf['aws_zones'] = form.aws_zones.data
-        return redirect('/gen/nets/%s' % conf_to_url(conf))
+        return redirect(url_for('.gen_networks', raw_conf=conf_to_url(conf)))
 
     return render_template('question.html',
                            form=form,
                            render_conf=render_conf(conf),
                            help_text=HELP_TEXT_AWS_ZONES,
-                           action=request.path)
+                           action=url_for('.aws_zones',
+                                          raw_conf=raw_conf))
 
 
 @app.route('/gen/nets/<path:raw_conf>', methods=['GET', 'POST'])
@@ -410,12 +603,13 @@ def gen_networks(raw_conf):
 
     num_networks = len(conf['networks'])
 
-    try:
-        # Test if we could handle one more network
-        calculate_num_groups(conf, num_networks=num_networks+1)
-    except:
-        # Reached the limit...
-        return redirect('/done/%s' % conf_to_url(conf))
+    if conf['is_aws']:
+        try:
+            # Test if we could handle one more network
+            calculate_num_groups(conf, num_networks=num_networks+1)
+        except:
+            # Reached the limit...
+            return redirect(url_for('.done', raw_conf=conf_to_url(conf)))
 
     if request.method == "GET":
         form = AddNetworkForm(conf=conf, net_name="net-%d" % num_networks)
@@ -424,7 +618,7 @@ def gen_networks(raw_conf):
         if form.cancel.data:
             # Processing the cancel button ahead of form validation, because we
             # want to allow it to be pressed even if form isn't filled out.
-            return redirect('/done/%s' % conf_to_url(conf))
+            return redirect(url_for('.done', raw_conf=conf_to_url(conf)))
 
     if form.validate_on_submit():
         cidr       = form.net_cidr.data
@@ -432,7 +626,7 @@ def gen_networks(raw_conf):
         block_mask = form.block_mask.data
         conf["networks"].append({"cidr" : cidr, "name" : name,
                                  "block_mask" : block_mask})
-        return redirect('/gen/nets/%s' % conf_to_url(conf))
+        return redirect(url_for('.gen_networks', raw_conf=conf_to_url(conf)))
 
     if num_networks == 0:
         table_title = "Provide information for a network:"
@@ -446,7 +640,8 @@ def gen_networks(raw_conf):
                            render_conf=render_conf(conf),
                            help_text=HELP_TEXT_NETWORK,
                            show_cancel=num_networks > 0,
-                           action=request.path)
+                           action=url_for('.gen_networks',
+                                          raw_conf=raw_conf))
 
 
 @app.route('/done/<path:raw_conf>', methods=['GET'])
@@ -463,9 +658,9 @@ def done(raw_conf):
     # Making a safe encoding for the URL. Note the 'decode' in the very end.
     # That's to remove the annoying   b'...'  formatting around the utf-8
     # encoded byte sequence.
-    download_link = "/download/%s" % \
-                    base64.urlsafe_b64encode(
-                        conf_to_url(conf).encode("utf=8")).decode()
+    download_link = url_for(".download",
+                            raw_conf=base64.urlsafe_b64encode(
+                                conf_to_url(conf).encode("utf=8")).decode())
 
     return render_template('done.html',
                            topo=json.dumps(topo, indent=4),
